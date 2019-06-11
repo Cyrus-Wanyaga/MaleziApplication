9651c3b5e9ee4a8d8bc3a0d7b84842d05b472693
Cyrus-Wanyaga <cwanyaga95@gmail.com>
2019-06-11 12:44:12 +0300

    added milestone 1 xml file

---
+++ app/src/main/java/com/example/maleziapplication/views/Milestone1.java
@@ -0,0 +1,29 @@
+package com.example.maleziapplication.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.example.maleziapplication.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class Milestone1 extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_milestone1);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_batr, menu);
+        return true;
+    }*/
+}
--- app/src/main/java/com/example/maleziapplication/views/MilestoneActivity.java
+++ app/src/main/java/com/example/maleziapplication/views/MilestoneActivity.java
@@ -1,16 +1,38 @@
 package com.example.maleziapplication.views;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.maleziapplication.R;
 
 public class MilestoneActivity extends Activity {
 
+    private Button btnMilestone1;
+    private Button btnMilestone2;
+    private Button btnMilestone3;
+    private Button btnMilestone4;
+    private Button btnMilestone5;
+    private Button btnMilestone6;
+    private Button btnMilestone7;
+    private Button btnMilestone8;
+    private Button btnMilestone9;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_milestones);
+
+        btnMilestone1 = findViewById(R.id.milestone1_btn);
+        btnMilestone1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent milestone1_Intent = new Intent(MilestoneActivity.this, Milestone1.class);
+                startActivity(milestone1_Intent);
+                finish();
+            }
+        });
     }
 }
